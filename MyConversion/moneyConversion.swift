f2752cdecfab3bb0dfdb78130935b12db1d85d6e
Nirusan Manoharan <nirusanmanoharan@gmail.com>
2025-03-28 00:01:11 -0400

    added a nav bar selector

---
+++ MyConversion/moneyConversion.swift
@@ -0,0 +1,18 @@
+//
+//  moneyConversion.swift
+//  MyConversion
+//
+//  Created by Nirusan Manoharan on 2025-03-27.
+//
+
+import SwiftUI
+
+struct moneyConversion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    moneyConversion()
+}
---
+++ MyConversion/weightConversion.swift
@@ -0,0 +1,18 @@
+//
+//  weightConversion.swift
+//  MyConversion
+//
+//  Created by Nirusan Manoharan on 2025-03-27.
+//
+
+import SwiftUI
+
+struct weightConversion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    weightConversion()
+}
